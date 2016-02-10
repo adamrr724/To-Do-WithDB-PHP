ca743e2ea3c4098d26896e916aa66cc0da3d9b6b
Jordan Meier and Ronando Long <jbirdmeier@gmail.com>
2016-02-10 08:50:29 -0800

    add session start method to app.php

--- app/app.php
+++ app/app.php
@@ -2,6 +2,14 @@
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/Task.php";
 
+    session_start();
+    if (empty($_SESSION['list_of_tasks'])) {
+        $_SESSION['list_of_tasks'] = array();
+    }
+
+
+
+
     $app = new Silex\Application();
 
     $app->get("/", function() {
@@ -13,7 +21,7 @@
         $output = "";
 
         foreach ($list_of_tasks as $task) {
-            $output .= "<p>" . $task->getDescription() . "</p>";
+            $output .= "<h2>" . $task->getDescription() . "</h2>";
         }
 
         return $output;
