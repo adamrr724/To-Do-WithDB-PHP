a0f7363247dc9349fb1109176683e8154782dea8
Jordan Meier and matt knutson <mknut001@gmail.com>
2016-02-22 10:40:21 -0800

    add initial files and folders

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -23,6 +23,10 @@ class ComposerAutoloaderInit28680d0b381cb2fb588c507f781355fc
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit28680d0b381cb2fb588c507f781355fc', 'loadClassLoader'));
 
+        $includePaths = require __DIR__ . '/include_paths.php';
+        array_push($includePaths, get_include_path());
+        set_include_path(join(PATH_SEPARATOR, $includePaths));
+
         $map = require __DIR__ . '/autoload_namespaces.php';
         foreach ($map as $namespace => $path) {
             $loader->set($namespace, $path);
