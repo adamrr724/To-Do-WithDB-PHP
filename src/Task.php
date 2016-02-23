d567df290c00dd45c25415eaf50cf30e6a261f30
Adam Ross Russell and Jordan Meier <jbirdmeier@gmail.com>
2016-02-23 08:28:55 -0800

    this will update TaskTest and Task class to reflect inclusion of categorty_id

--- src/Task.php
+++ src/Task.php
@@ -1,65 +1,73 @@
 <?php
-class Task
-{
-    private $description;
-    private $id;
-
-    function __construct($task_description, $task_id = null)
+    class Task
     {
-        $this->description = $task_description;
-        $this->id = $task_id;
-    }
+        private $description;
+        private $category_id;
+        private $id;
 
-    function SetDescription($new_description)
-    {
-        $this->description = (string) $new_description;
-    }
-
-    function GetDescription()
-    {
-        return $this->description;
-    }
-
-    function getId()
-    {
-        return $this->id;
-    }
-
-    function save()
-    {
-        $GLOBALS['DB']->exec("INSERT INTO tasks (description) VALUES ('{$this->GetDescription()}')");
-        $this->id = $GLOBALS['DB']->lastInsertId();
-    }
-
-    static function getAll()
-    {
-        $returned_tasks = $GLOBALS['DB']->query("SELECT * FROM tasks;");
-        $tasks = array();
-        foreach($returned_tasks as $task) {
-            $description = $task['description'];
-            $id = $task['id'];
-            $new_task = new Task($description, $id);
-            array_push($tasks, $new_task);
+        function __construct($description, $id = null, $category_id)
+        {
+            $this->description = $description;
+            $this->id = $id;
+            $this->category_id = $category_id;
         }
-        return $tasks;
-    }
 
-    static function deleteAll()
-    {
-        $GLOBALS['DB']->exec("DELETE FROM tasks;");
-    }
+        function setDescription($new_description)
+        {
+            $this->description = (string) $new_description;
+        }
 
-    static function find($search_id)
-    {
-        $found_task = null;
-        $tasks = Task::getAll();
-        foreach($tasks as $task) {
-           $task_id = $task->getId();
-           if ($task_id == $search_id) {
-               $found_task = $task;
-           }
-       }
-        return $found_task;
+        function getDescription()
+        {
+            return $this->description;
+        }
+
+        function getId()
+        {
+            return $this->id;
+        }
+
+        function getCategoryId()
+        {
+            return $this->category_id;
+        }
+
+        function save()
+        {
+            $GLOBALS['DB']->exec("INSERT INTO tasks (description, category_id) VALUES ('{$this->getDescription()}', {$this->getCategoryId()})");
+            $this->id = $GLOBALS['DB']->lastInsertId();
+        }
+
+        static function getAll()
+        {
+            $returned_tasks = $GLOBALS['DB']->query("SELECT * FROM tasks;");
+            $tasks = array();
+            foreach($returned_tasks as $task) {
+                $description = $task['description'];
+                $id = $task['id'];
+                $category_id = $task['category_id'];
+                $new_task = new Task($description, $id, $category_id);
+                array_push($tasks, $new_task);
+            }
+            return $tasks;
+        }
+
+        static function deleteAll()
+        {
+          $GLOBALS['DB']->exec("DELETE FROM tasks;");
+        }
+
+        static function find($search_id)
+        {
+            $found_task = null;
+            $tasks = Task::getAll();
+            foreach($tasks as $task) {
+                $task_id = $task->getId();
+                if ($task_id == $search_id) {
+                  $found_task = $task;
+                }
+            }
+            return $found_task;
+        }
     }
-}
 ?>
